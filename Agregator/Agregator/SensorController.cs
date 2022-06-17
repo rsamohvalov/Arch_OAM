f8b44fad853f2a679749bef1cb7f46e53380672e
rsamohvalov <r.samokhvalov@kryptonite.ru>
2022-06-17 16:56:51 +0300

    Agregator

---
+++ Agregator/Agregator/SensorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Sensor;
+
+namespace Agregator {
+    public class SensorController {
+        private ISensorReader SensorReader;
+        public SensorController( ISensorReader reader) {
+            SensorReader = reader;
+        }
+        public SensorDTO ReadSensorData(string id) {
+            return SensorReader.ReadBySensorId( id );
+        }
+    }
+    public class Agregator {
+        private SensorController Sensors;
+        private string Id;
+        public Agregator( string id, SensorController controller ) {
+            Id = id;
+            Sensors = controller;
+        }
+    }
+}
