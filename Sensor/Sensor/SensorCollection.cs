211e02e8bfe426de64499a14997a9b2192b5b886
rsamohvalov <r.samokhvalov@kryptonite.ru>
2022-06-17 15:54:59 +0300

    SensorCollection

---
+++ Sensor/Sensor/SensorCollection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sensor {
+    public class SensorCollection : ISensorReader {
+        private List<Sensor> SensorsList;
+        Random rand;
+        public SensorCollection() {
+            SensorsList = new List<Sensor>();
+        }
+        public void AddSensor( SensorProvider Provider, string id ) {
+           SensorsList.Add( Provider.CreateSensor( id ) );
+        }
+        public void AddSensor( SensorProvider Provider ) {
+            SensorsList.Add( Provider.CreateSensor( rand.Next().ToString() ) );
+        }
+        public SensorDTO ReadBySensorId( string id ) {
+            foreach( Sensor sensor in SensorsList ) {
+                if( sensor.GetId() == id ) {
+                    return sensor.ReadValue();
+                }
+            }
+            return null;
+        }
+        public List<SensorDTO> ReadAllSensors() {
+            List<SensorDTO> returnList = new List<SensorDTO>();
+            foreach(Sensor sensor in SensorsList) {
+                returnList.Add( sensor.ReadValue());                
+            }
+            return returnList;
+        }
+    }
+}
