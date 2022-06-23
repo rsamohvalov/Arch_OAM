6a94d96512fd65229dbf780d9ff12536b06a0d8d
rsamohvalov <r.samokhvalov@kryptonite.ru>
2022-06-23 17:28:14 +0300

    Added EntityFramework

---
+++ Sensor/WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication.Models {
+    public class ErrorViewModel {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty( RequestId );
+    }
+
+    public class User {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; } 
+        public string Login { get; set; }
+        public string PasswordHash { get; set; }
+    }
+
+    public class ApplicationContext : DbContext {
+        public DbSet<User> Users { get; set; } = null!;
+        public ApplicationContext( DbContextOptions<ApplicationContext> options )
+            : base( options ) {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+    }
+}
